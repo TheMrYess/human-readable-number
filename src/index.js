dc538bd241b589aa3d3cecbc84eae692f4b879ce
TheMrYess <79982962+TheMrYess@users.noreply.github.com>
2021-03-12 02:54:46 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function toReadable (number) {
-  
+    var ones = new Array('', ' one', ' two', ' three', ' four', ' five', ' six', ' seven', ' eight', ' nine', ' ten', ' eleven', ' twelve', ' thirteen', ' fourteen', ' fifteen', ' sixteen', ' seventeen', ' eighteen', ' nineteen');
+    var tens = new Array('', '', ' twenty', ' thirty', ' forty', ' fifty', ' sixty', ' seventy', ' eighty', ' ninety');
+    var n_str=String(number),
+    ans;
+    if (number==0) return "zero";
+    if (number<20)
+    {
+    return ones[number].slice(1);
+    }
+    else if(number<100)
+    {
+    ans=(tens[Number(n_str.charAt(0))]);
+    ans+=(ones[Number(n_str.charAt(1))]);
+    ans=ans.slice(1);
+    return ans;
+   // ans=tens[]
+    } else if(number%100<20)
+    {
+   // console.log(n%100)
+    ans=(ones[Number(n_str.charAt(0))])+" hundred";
+    ans+=(ones[number%100]);
+    ans=ans.slice(1);
+    return ans;
+    } 
+    
+    ans=(ones[Number(n_str.charAt(0))])+" hundred";
+    ans+=(tens[Number(n_str.charAt(1))]);
+    ans+=(ones[Number(n_str.charAt(2))]);
+    ans=ans.slice(1);
+    return ans;
+    
 }
